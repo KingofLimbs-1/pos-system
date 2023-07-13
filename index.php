3c32bcdccfdbb1530d567184c05724629beef8ec
Miles Pieterse <milesyusuf123@gmail.com>
2023-07-13 22:45:49 +0200

    Update: logic for menuItem display + built confirm order button + error handling logic

--- index.php
+++ index.php
@@ -1,25 +1,63 @@
 <?php
+include 'data/data.php';
+include 'model/MenuItem.php';
+?>
 
-// display error codes and messages
+<?php
+// Variables
+$menuItems = [];
+// ...
+?>
 
-use function PHPSTORM_META\type;
+<!-- Session variable -->
+<?php
+$errors = [];
+$_SESSION["errors"] = $errors;
+?>
+<!-- Session variable end -->
 
+
+<!-- Error handling -->
+<?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+?>
 
-session_start();
-
-// Session variables
-$items = $_SESSION['items'];
-// ...
-
-
-if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-    // Order total
-    // ...
+<?php
+if (!empty($errors)) {
+    foreach ($errors as $error) {
+        echo $error . "</br>";
+    }
 }
 ?>
+
+<?php
+if (!isset($menuItems)) {
+    $errors[] = "menuItems class is not populating menuItems array";
+} else {
+    // Proceed as normal...
+}
+?>
+<!-- Error handling end -->
+
+
+<!-- Data handling -->
+<?php
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    if (isset($_POST['selectedItemValue'])) {
+    }
+}
+?>
+
+<?php
+// MenuItem object instantiations
+foreach ($items as $item) {
+    $menuItems[] = new MenuItem($item['name'], $item['newPrice'], $item['oldPrice'], $item['barcode']);
+}
+// ...
+?>
+<!-- Data handling end -->
 
 
 <!DOCTYPE html>
@@ -48,35 +86,39 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
                 Amount: R <span>0</span>
             </span>
         </div>
+        <div>
+            <form action="" method="post">
+                <input class="confirmBtn" type="submit" name="confirmBtn" value="Confirm Order">
+            </form>
+        </div>
     </div>
 
     <hr>
 
     <section>
-        <form class="items" action=" <?php $_SERVER['PHP_SELF'] ?>" method="post">
+        <form class="items" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
 
-            <?php foreach ($items as $key => $value) {
-            ?>
+            <?php foreach ($menuItems as $menuItem) { ?>
                 <button type="submit" name="selectedItemValue" value="" class="item">
-                    <span>
-                        <?php echo $value['name']; ?>
+                    <div>
+                        <span><?php echo $menuItem->getName(); ?></span>
                         <br>
-                        <?php echo "R" . $value['price']; ?>
-                    </span>
+                        <span class="oldPrice"><?php echo "R" . $menuItem->getOldPrice(); ?></span>
+                        <span><?php echo "R" . $menuItem->getNewPrice(); ?></span>
+                    </div>
                 </button>
-            <?php
-            }
-            ?>
+            <?php } ?>
         </form>
     </section>
 
-    <form action="./views/checkout.php" method="get" class="checkout">
+
+    <form action="views/checkout.php" method="get" class="checkout">
         <input type="hidden" name="subTotal" value="sub total amount">
         <button type="submit">
             Proceed to payment
         </button>
     </form>
 
-
 </body>
+
 </html>
