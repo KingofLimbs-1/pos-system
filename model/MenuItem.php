3261c84aff6d0b41d68203a40139b0422beeb455
Miles Pieterse <milesyusuf123@gmail.com>
2023-07-13 21:25:10 +0200

    Add: menuItem class that is responsible for creating store items

--- model/MenuItem.php
+++ model/MenuItem.php
@@ -1,13 +1,35 @@
 <?php
 
-class MenuItem {
+class MenuItem
+{
 
     // fields
-
+    private $name;
+    private $price;
+    private $barcode;
 
     // constructor
-
+    public function __construct($name, $price, $barcode)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->barcode = $barcode;
+    }
 
     // methods
-    
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function getBarcode()
+    {
+        return $this->barcode;
+    }
 }
+
