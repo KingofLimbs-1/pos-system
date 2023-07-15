138b4fa57876d4c81166eb38fa56cd8886a32290
Miles Pieterse <milesyusuf123@gmail.com>
2023-07-15 22:53:01 +0200

    Add: addItem() function that handles adding order to array & calculating price of order

--- include/addItem.php
+++ include/addItem.php
@@ -1,8 +1,13 @@
 <?php
 
-function addItem(MenuItem $menuItem) {
-
-    // business logic here..
-
-    return;
+function addItem(MenuItem $menuItem)
+{
+    // Order array instantiation
+    if (!isset($_SESSION["order"])) {
+        $_SESSION["order"] = [];
+    }
+    // Order population
+    $_SESSION["order"][] = $menuItem;
+    // Order price creation
+    $_SESSION["orderTotal"] += $menuItem->getNewPrice();
 }
