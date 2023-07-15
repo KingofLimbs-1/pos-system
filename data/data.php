c1c6f2177409743a2072f141d394f771cf4c00ee
Miles Pieterse <milesyusuf123@gmail.com>
2023-07-15 22:55:34 +0200

    Update: Created img properties for items for product display purposes

--- data/data.php
+++ data/data.php
@@ -1,59 +1,67 @@
 <?php
-session_start();
 
 $items = [
     [
         "name" => "LG TV",
-        "newPrice" => 1000,
-        "oldPrice" => 2000,
+        "newPrice" => 10000,
+        "oldPrice" => 20000,
+        "src" => "../pos-system/static/img/lgTV.png",
         "barcode" => 18367
     ],
     [
         "name" => "Defy Kettle",
         "newPrice" => 500,
         "oldPrice" => 1000,
+        "src" => '../pos-system/static/img/kettle.png',
         "barcode" => 23111
     ],
     [
         "name" => "HiSense Headset",
         "newPrice" => 800,
         "oldPrice" => 1600,
+        "src" => '../pos-system/static/img/headset.png',
         "barcode" => 14837
     ],
     [
         "name" => "Samsung Microwave",
         "newPrice" => 1000,
         "oldPrice" => 2000,
+        "src" => '../pos-system/static/img/microwave.png',
         "barcode" => 30921
     ],
     [
         "name" => "JVC Home Theatre System",
         "newPrice" => 7000,
         "oldPrice" => 14000,
+        "src" => '../pos-system/static/img/homeTheatre.png',
         "barcode" => 83120
     ],
     [
         "name" => "Radio JVC",
         "newPrice" => 200,
         "oldPrice" => 400,
+        "src" => '../pos-system/static/img/radio.png',
         "barcode" => 82180
     ],
     [
         "name" => "Samsung Washing Machine",
         "newPrice" => 3000,
         "oldPrice" => 6000,
+        "src" => '../pos-system/static/img/washingMachine.png',
         "barcode" => 30299
     ],
     [
         "name" => "Telefunken TV",
         "newPrice" => 3000,
         "oldPrice" => 6000,
+        "src" => '../pos-system/static/img/Telefunken.png',
         "barcode" => 31204
     ],
     [
         "name" => "Philips Air Fryer",
         "newPrice" => 900,
         "oldPrice" => 1800,
+        "src" => '../pos-system/static/img/airFryer.png',
         "barcode" => 13204
     ],
 ];
