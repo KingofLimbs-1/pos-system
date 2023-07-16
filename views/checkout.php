922314a88e23caa8e58310b65f17ed85620a7d5a
Miles Pieterse <milesyusuf123@gmail.com>
2023-07-16 23:50:46 +0200

    Add: Order to checkout page + styling for page

--- views/checkout.php
+++ views/checkout.php
@@ -1,11 +1,13 @@
 <?php
+include '../data/data.php';
+include '../model/MenuItem.php';
+include '../include/addItem.php';
+?>
 
-// display error codes and messages
+<?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-session_start();
 
 // redirect back to index if payment button is selected
 if (isset($_GET['payment'])) {
@@ -14,44 +16,60 @@ if (isset($_GET['payment'])) {
 }
 ?>
 
+<?php
+// Variables
+if (isset($_SESSION["order"]) && isset($_SESSION["orderTotal"])) {
+    $order = $_SESSION["order"];
+    $orderTotal = $_SESSION["orderTotal"];
+}
+// ...
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>S&S POS | Pay</title>
-    <link rel="stylesheet" href="./../static/css/style.css">
+    <link rel="stylesheet" href="./../static/css/checkout.css">
 </head>
+
 <body>
-    <h1>
-        <span style="color:red">Select</span> and <span style="color:blue">Save</span>
+    <h1 class="heading">
+        Select and Save
     </h1>
 
     <hr>
 
-    <h2>Items Purchased:</h2>
+    <div class="purchasedItems">
+        <h2>Items Purchased</h2>
+        <ul>
+            <?php if (isset($order)) {
+                foreach ($order as $menuItem) { ?>
+                    <li><?php echo $menuItem->getName(); ?></li>
+                <?php } ?>
+            <?php } ?>
+        </ul>
+    </div>
+    <hr>
 
-    <ul>
-        <li>item 1</li>
-        <li>item 2</li>
-        <li>item 3</li>
-    </ul>
+    <h2 class="amounts">
+        <?php if (isset($orderTotal)) { ?>
+            Amount: R<span><?php echo $orderTotal; ?></span>
+            <br>
+            Subtotal (excluding VAT):  R<span>0.00</span>
+            <br>
+            Total:  R<span>0.00</span>
+        <?php } ?>
+    </h2>
 
     <hr>
 
-    <h2>
-        Amount: R<span>0.00</span>
-        <br>
-        VAT Amount: R <span>0.00</span>
-        <br>
-        <br>
-        Subtotal for all items: R<span>0.00</span>
-    </h2>
-
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
-        <button style="background-color:red" type="submit" name="payment">Pay with card</button>
-        <button style="background-color:cornflowerblue" type="submit" name="payment">Pay with cash</button>
+        <button type="submit" name="payment">Pay with card</button>
+        <button type="submit" name="payment">Pay with cash</button>
     </form>
 
 </body>
