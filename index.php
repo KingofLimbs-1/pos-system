3f00fa91b941ac7fac0e7f4c177053c348ac8d80
Miles Pieterse <milesyusuf123@gmail.com>
2023-07-17 01:45:14 +0200

    Update: Built Order population logic + input validations + static elements

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <?php
 include 'data/data.php';
 include 'model/MenuItem.php';
+include 'include/addItem.php';
 ?>
-
-<?php
-// Variables
-$menuItems = [];
-// ...
-?>
-
-<!-- Session variable -->
-<?php
-$errors = [];
-$_SESSION["errors"] = $errors;
-?>
-<!-- Session variable end -->
-
 
 <!-- Error handling -->
+
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -25,38 +13,77 @@ error_reporting(E_ALL);
 ?>
 
 <?php
+if (isset($_POST['confirmBtn'])) {
+    if (empty($_SESSION["order"])) {
+        $errors[] = "Please add items to your cart";
+    } else {
+        // ...
+    }
+}
+?>
+
+<?php
+if (isset($_POST['confirmBtn'])) {
+    if (empty($_SESSION["order"])) {
+        $errors[] = "Please add items to your cart";
+    } else {
+        // ...
+    }
+}
+?>
+
+<?php
+// Check to see if any errors occcured
 if (!empty($errors)) {
     foreach ($errors as $error) {
         echo $error . "</br>";
     }
 }
+// ...
 ?>
 
-<?php
-if (!isset($menuItems)) {
-    $errors[] = "menuItems class is not populating menuItems array";
-} else {
-    // Proceed as normal...
-}
-?>
 <!-- Error handling end -->
 
-
 <!-- Data handling -->
-<?php
-if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-    if (isset($_POST['selectedItemValue'])) {
-    }
-}
-?>
 
 <?php
 // MenuItem object instantiations
 foreach ($items as $item) {
-    $menuItems[] = new MenuItem($item['name'], $item['newPrice'], $item['oldPrice'], $item['barcode']);
+    $menuItems[] = new MenuItem($item['name'], $item['newPrice'], $item['oldPrice'], $item['src'], $item['barcode']);
 }
 // ...
 ?>
+
+<?php
+// Add menuItem to order
+$selectedItem = $_POST["selectedItemValue"] ?? [];
+if (!empty($selectedItem)) {
+    foreach ($selectedItem as $item) {
+        // Compare the trimmed name of each MenuItem object with the trimmed selected item
+        $filteredItems = array_filter($menuItems, function ($menuItem) use ($item) {
+            return trim($menuItem->getName()) === trim($item);
+        });
+
+        if (!empty($filteredItems)) {
+            // Retrieves first matched MenuItem object and get its new price
+            $selectedPrice = reset($filteredItems)->getNewPrice();
+            // Add the matched MenuItem to order using the addItem() function
+            addItem(reset($filteredItems), $selectedPrice);
+        }
+    }
+}
+// ...
+?>
+
+<?php
+// Clear order button
+if (isset($_POST["clearBtn"])) {
+    $_SESSION["order"] = [];
+    $_SESSION["orderTotal"] = 0;
+}
+// ...
+?>
+
 <!-- Data handling end -->
 
 
@@ -78,12 +105,18 @@ foreach ($items as $item) {
         </h1>
     </div>
 
+    <form action="" method="post">
+        <?php if (!empty($_SESSION["order"])) : ?>
+            <input class="clearBtn" type="submit" name="clearBtn" value="Clear Order">
+        <?php endif; ?>
+    </form>
+
     <hr>
 
     <div class="till__display">
         <div>
             <span class="till__console">
-                Amount: R <span>0</span>
+                Amount: R <span><?php echo isset($_SESSION["orderTotal"]) ? $_SESSION["orderTotal"] : 0; ?></span>
             </span>
         </div>
         <div>
@@ -99,25 +132,31 @@ foreach ($items as $item) {
         <form class="items" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
 
             <?php foreach ($menuItems as $menuItem) { ?>
-                <button type="submit" name="selectedItemValue" value="" class="item">
+                <button type="submit" name="selectedItemValue[]" value="<?php echo $menuItem->getName(); ?>" class="item">
                     <div>
                         <span><?php echo $menuItem->getName(); ?></span>
                         <br>
                         <span class="oldPrice"><?php echo "R" . $menuItem->getOldPrice(); ?></span>
                         <span><?php echo "R" . $menuItem->getNewPrice(); ?></span>
+                        <br>
+                        <img src="<?php echo $menuItem->getImgSrc(); ?>" alt="itemImage"></span>
+                        <br>
+                        <span><?php echo $menuItem->getBarcode(); ?></span>
+                        <br>
                     </div>
                 </button>
             <?php } ?>
         </form>
     </section>
 
-
-    <form action="views/checkout.php" method="get" class="checkout">
-        <input type="hidden" name="subTotal" value="sub total amount">
-        <button type="submit">
-            Proceed to payment
-        </button>
-    </form>
+    <?php if (isset($_POST['confirmBtn']) && !empty($_SESSION['order'])) : ?>
+        <form action="../pos-system/views/checkout.php" method="get" class="checkout">
+            <input type="hidden" name="subTotal" value="sub total amount">
+            <button type="submit">
+                Proceed to payment
+            </button>
+        </form>
+    <?php endif; ?>
 
 </body>
 
